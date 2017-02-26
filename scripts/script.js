c79bfd6b4200eab3365901da5af4a5966d740a6a
Luming Wu <reallumingwu@gmail.com>
2017-02-25 23:44:02 -0500

    Hello

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+    /* User display  */
+    var logindisplay = $("#logindisplay");
+    var userdisplay = $("#userdisplay");
+    var name = $("#name");
 
+    userdisplay.hide();
+
+    $("#loginbutton").click(function(){
+        logindisplay.hide();
+        userdisplay.show();
+
+
+        name.text("User");
+    });
+
+    $("#logoutbutton").click(function(){
+        userdisplay.hide();
+        logindisplay.show();
+
+
+        name.text("");
+    });
+    $("#weekly-whole-table > tr > th > td")
 });
